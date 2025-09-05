7a7bec7a30afd2e51610e7d6b5a08283b08e4cb7
Carter Williams <carterww@hotmail.com>
2025-09-05 13:00:16 -0500

    Rename funcs to lf_ring_<type>_<op>, wrap reused funcs in funcs

--- include/lf_ring.h
+++ include/lf_ring.h
@@ -13,8 +13,61 @@
 #include <lf_op.h>
 #include <lf_platform.h>
 
-#define IS_POWER_OF_TWO(x) ((x) && (((x) & ((x) - 1)) == 0))
-#define MIN(a, b) ((a) > (b) ? (b) : (a))
+/* TODO: Make this a build time config param */
+#define LF_RING_CACHELINE_PAD (64)
+
+/* lf_ring allows you to use a custom data type that is contained in each
+ * slot. If none is provided, a union of generic pointers is stored in
+ * the buffer.
+ *
+ * In order to define your own slot data type, you must do the following
+ * before including lf_ring.h:
+ * 1. Define LF_RING_CUSTOM_SLOT_DATA.
+ * 2. Implement a type named "lf_ring_slot_data"
+ * 3. Implement a function named "lf_ring_store_slot_data" that stores
+ *    data into slot's data.
+ * 4. Implement a function named "lf_ring_load_slot_data" that places
+ *    the slot's data into data.
+ * You must ensure the store and load functions actually write to memory.
+ * Using a normal store like "slot->ptr = data->ptr" is not sufficient
+ * because the compiler may optimize this in a way where data->ptr isn't
+ * actually written to the slot's memory. Use lf_op.h's load and store
+ * primitives.
+ *
+ * It is also noteworthy that the load and store does not have to be
+ * a single load or store operation. You can perform multiple if you
+ * are working with structs or arrays.
+ *
+ * NOTE: Using a custom data type doesn't change the function names.
+ * This means you can only use 1 lf_ring.h custom implementation per
+ * translation unit. If you use a custom data type, I highly recommend
+ * you don't put the lf_ring calls in a header file.
+ */
+#if !defined(LF_RING_CUSTOM_SLOT_DATA)
+
+typedef union lf_ring_slot_data lf_ring_slot_data;
+
+union lf_ring_slot_data {
+	void *ptr;
+	uintptr_t uptr;
+	intptr_t iptr;
+};
+
+LF_ATTR_ALWAYS_INLINE
+static void lf_ring_store_slot_data(union lf_ring_slot_data *slot,
+				    const union lf_ring_slot_data *data)
+{
+	lf_op_store_ptr(&slot->ptr, data->ptr);
+}
+
+LF_ATTR_ALWAYS_INLINE
+static void lf_ring_load_slot_data(union lf_ring_slot_data *slot,
+				   union lf_ring_slot_data *data)
+{
+	data->ptr = lf_op_load_ptr(&slot->ptr);
+}
+
+#endif /* !LF_RING_CUSTOM_SLOT_DATA */
 
 /* There are three main implementations:
  * 1. Single-Producer, Single-Consumer (spsc): 1 thread enqueues data and
@@ -24,8 +77,8 @@
  * 3. Multiple-Producer, Single/Multiple-Consumer (mpsc, mpmc): 1+ thread
  *    enqueues data and 1+ threads dequeues data.
  *
- * Each type has their own functions with a suffix of spsc, spmc, mpsc,
- * or mpmc. You should use those functions; you should not use spsc functions
+ * Each type has their own functions with spsc, spmc, mpsc, or mpmc in the
+ * name. You should use those functions; you should not use spsc functions
  * for a spmc queue (even though they have the same underlying type).
  * 
  * Cases (1) and (2) can use the same underlying struct, but case (3)
@@ -40,27 +93,53 @@ typedef struct lf_ring_sp lf_ring_spmc;
 typedef struct lf_ring_mp lf_ring_mpsc;
 typedef struct lf_ring_mp lf_ring_mpmc;
 
+typedef struct lf_ring_sp_slot lf_ring_spsc_slot;
+typedef struct lf_ring_sp_slot lf_ring_spmc_slot;
 typedef struct lf_ring_mp_slot lf_ring_mpsc_slot;
 typedef struct lf_ring_mp_slot lf_ring_mpmc_slot;
 
-struct lf_ring_sp {
-	size_t head;
-	size_t tail;
-	size_t mask;
-	void **buf;
+struct lf_ring_sp_slot {
+	lf_ring_slot_data data;
 };
 
 struct lf_ring_mp_slot {
 	size_t seq;
-	void *data;
+	lf_ring_slot_data data;
+};
+
+struct lf_ring_sp {
+	size_t head;
+	char _pad[LF_RING_CACHELINE_PAD - sizeof(size_t)];
+	size_t tail;
+	size_t mask;
+	struct lf_ring_sp_slot *buf;
 };
 
 struct lf_ring_mp {
 	size_t head;
+	char _pad[LF_RING_CACHELINE_PAD - sizeof(size_t)];
 	size_t tail;
 	size_t mask;
 	struct lf_ring_mp_slot *buf;
 };
+
+/* In the case of mpsc and mpmc, this length calculation is only an estimate
+ * because tail and head don't directly correspond to the state of the data
+ * in the queue.
+ *
+ * The tail is incremented before storing the data, and the head is incremented
+ * before loading the data. It may be the case that tail == head but none of the
+ * consumers have actually loaded the data. Or that tail - head == cap but none
+ * of the producers have stored the data.
+ */
+#define LF_RING_LEN(ring_ptr)                 \
+	(lf_op_load_size(&(ring_ptr)->tail) - \
+	 lf_op_load_size(&(ring_ptr)->head))
+
+#define LF_RING_CAP(ring_ptr) ((ring_ptr)->mask + 1)
+
+#define IS_POWER_OF_TWO(x) ((x) && (((x) & ((x) - 1)) == 0))
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
 
 /* Initialize the ring buffer with a buffer of length buffer_len. buffer_len
  * must be a power of two.
@@ -69,7 +148,7 @@ struct lf_ring_mp {
  * power of 2.
  */
 LF_ATTR_ALWAYS_INLINE
-static bool lf_ring_init_spsc(lf_ring_spsc *ring, void **buffer,
+static bool lf_ring_spsc_init(lf_ring_spsc *ring, lf_ring_spsc_slot *buffer,
 			      size_t buffer_len)
 {
 	if (!IS_POWER_OF_TWO(buffer_len)) {
@@ -82,29 +161,13 @@ static bool lf_ring_init_spsc(lf_ring_spsc *ring, void **buffer,
 	return true;
 }
 
-/* Returns the number of items in the ring */
-LF_ATTR_ALWAYS_INLINE
-static size_t lf_ring_len_spsc(lf_ring_spsc *ring)
-{
-	size_t t, h;
-	h = lf_op_load_size(&ring->head);
-	t = lf_op_load_size(&ring->tail);
-	return t - h;
-}
-
-/* Returns the maximum number of items the ring can store */
-LF_ATTR_ALWAYS_INLINE
-static size_t lf_ring_cap_spsc(lf_ring_spsc *ring)
-{
-	return ring->mask + 1;
-}
-
 /* Place a single item at the tail.
  *
  * Returns true if the item was added successfully, false if the ring
  * is full.
  */
-static bool lf_ring_enqueue_spsc(lf_ring_spsc *ring, void *item)
+static bool lf_ring_spsc_enqueue(lf_ring_spsc *ring,
+				 const lf_ring_slot_data *item)
 {
 	size_t t, h, idx;
 
@@ -114,7 +177,7 @@ static bool lf_ring_enqueue_spsc(lf_ring_spsc *ring, void *item)
 		return false;
 	}
 	idx = t & ring->mask;
-	lf_op_store_ptr(&ring->buf[idx], item);
+	lf_ring_store_slot_data(&ring->buf[idx].data, item);
 	/* Ensure new item visible before tail */
 	lf_fence_store();
 	/* Producer owns tail, no need for atomic RMW */
@@ -127,7 +190,7 @@ static bool lf_ring_enqueue_spsc(lf_ring_spsc *ring, void *item)
  * Returns true if there was an item to dequeue, false if the
  * queue is empty. *item is undefined if false is returned.
  */
-static bool lf_ring_dequeue_spsc(lf_ring_spsc *ring, void **item)
+static bool lf_ring_spsc_dequeue(lf_ring_spsc *ring, lf_ring_slot_data *item)
 {
 	size_t t, h, idx;
 
@@ -139,7 +202,7 @@ static bool lf_ring_dequeue_spsc(lf_ring_spsc *ring, void **item)
 	idx = h & ring->mask;
 	/* Ensure item will be visible */
 	lf_fence_load();
-	*item = lf_op_load_ptr(&ring->buf[idx]);
+	lf_ring_load_slot_data(&ring->buf[idx].data, item);
 	/* Ensure we load item before incrementing head so
 	 * producer can't overwrite idx.
 	 */
@@ -161,22 +224,22 @@ static bool lf_ring_dequeue_spsc(lf_ring_spsc *ring, void **item)
  * nitems fences would be issued. The only downside is the consumer may
  * see the first new items a bit later.
  */
-static size_t lf_ring_enqueue_many_spsc(lf_ring_spsc *ring, void **items,
+static size_t lf_ring_spsc_enqueue_many(lf_ring_spsc *ring,
+					const lf_ring_slot_data *items,
 					size_t nitems)
 {
-	size_t i, t, h, idx, cap, rem;
+	size_t i, t, h, idx, rem;
 
 	h = lf_op_load_size(&ring->head);
 	t = ring->tail;
-	cap = ring->mask + 1;
-	rem = cap - (t - h);
+	rem = LF_RING_CAP(ring) - (t - h);
 	nitems = MIN(nitems, rem);
 	if (nitems == 0) {
 		return 0;
 	}
 	for (i = 0; i < nitems; i += 1, t += 1) {
 		idx = t & ring->mask;
-		lf_op_store_ptr(&ring->buf[idx], items[i]);
+		lf_ring_store_slot_data(&ring->buf[idx].data, &items[i]);
 	}
 	lf_fence_store();
 	lf_op_store_size(&ring->tail, t);
@@ -194,14 +257,14 @@ static size_t lf_ring_enqueue_many_spsc(lf_ring_spsc *ring, void **items,
  * lf_ring_dequeue(), nitems * 2 fences would be issued. The only
  * downside is the producer may see the first empty slots a bit later.
  */
-static size_t lf_ring_dequeue_many_spsc(lf_ring_spsc *ring, void **items,
-					size_t nitems)
+static size_t lf_ring_spsc_dequeue_many(lf_ring_spsc *ring,
+					lf_ring_slot_data *items, size_t nitems)
 {
 	size_t i, t, h, idx, len;
 
 	h = ring->head;
 	t = lf_op_load_size(&ring->tail);
-	len = (t - h);
+	len = t - h;
 	nitems = MIN(nitems, len);
 	if (nitems == 0) {
 		return 0;
@@ -209,19 +272,27 @@ static size_t lf_ring_dequeue_many_spsc(lf_ring_spsc *ring, void **items,
 	lf_fence_load();
 	for (i = 0; i < nitems; i += 1, h += 1) {
 		idx = h & ring->mask;
-		items[i] = lf_op_load_ptr(&ring->buf[idx]);
+		lf_ring_load_slot_data(&ring->buf[idx].data, &items[i]);
 	}
 	lf_fence_load_store();
 	lf_op_store_size(&ring->head, h);
 	return nitems;
 }
 
-/* See spsc comments for more details */
-#define lf_ring_init_spmc lf_ring_init_spsc
-#define lf_ring_len_spmc lf_ring_len_spsc
-#define lf_ring_cap_spmc lf_ring_cap_spsc
-#define lf_ring_enqueue_spmc lf_ring_enqueue_spsc
-#define lf_ring_enqueue_many_spmc lf_ring_enqueue_many_spsc
+/* See spsc comments for more details on spmc functions that use spsc impl */
+
+LF_ATTR_ALWAYS_INLINE
+static bool lf_ring_spmc_init(lf_ring_spsc *ring, lf_ring_spsc_slot *buffer,
+			      size_t buffer_len)
+{
+	return lf_ring_spsc_init(ring, buffer, buffer_len);
+}
+
+static bool lf_ring_spmc_enqueue(lf_ring_spsc *ring,
+				 const lf_ring_slot_data *item)
+{
+	return lf_ring_spsc_enqueue(ring, item);
+}
 
 /* Same interface as lf_ring_dequeue_spsc with a different implementation
  * to handle contention at the head.
@@ -229,7 +300,7 @@ static size_t lf_ring_dequeue_many_spsc(lf_ring_spsc *ring, void **items,
  * The main difference compared to the spsc version is we now require a
  * CAS instead of a simple store to update the head.
  */
-static bool lf_ring_dequeue_spmc(lf_ring_spsc *ring, void **item)
+static bool lf_ring_spmc_dequeue(lf_ring_spsc *ring, lf_ring_slot_data *item)
 {
 	size_t t, h, idx;
 
@@ -242,7 +313,7 @@ static bool lf_ring_dequeue_spmc(lf_ring_spsc *ring, void **item)
 		idx = h & ring->mask;
 		/* Ensure item will be visible */
 		lf_fence_load();
-		*item = lf_op_load_ptr(&ring->buf[idx]);
+		lf_ring_load_slot_data(&ring->buf[idx].data, item);
 		/* Ensure we load item before incrementing head so
 		 * producer can't overwrite idx.
 		 */
@@ -254,56 +325,15 @@ static bool lf_ring_dequeue_spmc(lf_ring_spsc *ring, void **item)
 	return true;
 }
 
-/* See spsc comments for interface details and mpmc comments for implemenation
- * details.
- */
-#define lf_ring_init_mpsc lf_ring_init_mpmc
-#define lf_ring_len_mpsc lf_ring_len_mpmc
-#define lf_ring_cap_mpsc lf_ring_cap_mpmc
-#define lf_ring_enqueue_mpsc lf_ring_enqueue_mpmc
-
-/* Same interface as other dequeue functions with a different implementation
- * to handle producer slot contention.
- *
- * This version is the exact same as the mpmc version, but we replace the
- * CAS on ring->head with a relaxed store. See mpmc version for more comments
- * on implementation.
- */
-static bool lf_ring_dequeue_mpsc(lf_ring_mpmc *ring, void **item)
+static size_t lf_ring_spmc_enqueue_many(lf_ring_spsc *ring,
+					const lf_ring_slot_data *items,
+					size_t nitems)
 {
-	size_t h, h_next, seq, idx;
-	intptr_t diff_signed;
-	lf_ring_mpmc_slot *slot;
-
-	while (true) {
-		h = lf_op_load_size(&ring->head);
-		h_next = h + 1;
-		idx = h & ring->mask;
-		slot = &ring->buf[idx];
-		seq = lf_op_load_size(&slot->seq);
-
-		diff_signed = (intptr_t)seq - (intptr_t)h_next;
-		if (diff_signed == 0) {
-			/* Perform a relaxed store on head instead of CAS since no
-			 * other consumers can write to it.
-			 */
-			lf_op_store_size(&ring->head, h_next);
-		} else if (diff_signed > 0) {
-			continue;
-		} else {
-			return false;
-		}
-	}
-	lf_fence_load();
-
-	*item = lf_op_load_ptr(&slot->data);
-	lf_fence_load_store();
-	lf_op_store_size(&slot->seq, h_next + ring->mask);
-	return true;
+	return lf_ring_spsc_enqueue_many(ring, items, nitems);
 }
 
 LF_ATTR_ALWAYS_INLINE
-static bool lf_ring_init_mpmc(lf_ring_mpmc *ring, lf_ring_mpmc_slot *buffer,
+static bool lf_ring_mpsc_init(lf_ring_mpmc *ring, lf_ring_mpmc_slot *buffer,
 			      size_t buffer_len)
 {
 	size_t i;
@@ -325,38 +355,16 @@ static bool lf_ring_init_mpmc(lf_ring_mpmc *ring, lf_ring_mpmc_slot *buffer,
 	return true;
 }
 
-/* In the case of mpsc and mpmc, this length calculation is only an estimate
- * because tail and head don't directly correspond to the state of the data
- * in the queue.
- *
- * The tail is incremented before storing the data, and the head is incremented
- * before loading the data. It may be the case that tail == head but none of the
- * consumers have actually loaded the data. Or that tail - head == cap but none
- * of the producers have stored the data.
- */
-LF_ATTR_ALWAYS_INLINE
-static size_t lf_ring_len_mpmc(lf_ring_mpmc *ring)
-{
-	size_t t, h;
-	h = lf_op_load_size(&ring->head);
-	t = lf_op_load_size(&ring->tail);
-	return t - h;
-}
-
-LF_ATTR_ALWAYS_INLINE
-static size_t lf_ring_cap_mpmc(lf_ring_mpmc *ring)
-{
-	return ring->mask + 1;
-}
-
-/* The mpmc implementations were derived from Dmitry Vyukov's implementation
- * (https://www.1024cores.net/home/lock-free-algorithms/queues/bounded-mpmc-queue).
+/* The mpsc and mpmc implementations were derived from Dmitry Vyukov's
+ * implementation at
+ * https://www.1024cores.net/home/lock-free-algorithms/queues/bounded-mpmc-queue.
  */
 
 /* Same interface as other enqueue functions with a different implementation
  * to handle data races.
  */
-static bool lf_ring_enqueue_mpmc(lf_ring_mpmc *ring, void *item)
+static bool lf_ring_mpsc_enqueue(lf_ring_mpmc *ring,
+				 const lf_ring_slot_data *item)
 {
 	size_t t, seq, idx;
 	intptr_t diff_signed;
@@ -395,7 +403,7 @@ static bool lf_ring_enqueue_mpmc(lf_ring_mpmc *ring, void *item)
 		}
 	}
 
-	lf_op_store_ptr(&slot->data, item);
+	lf_ring_store_slot_data(&slot->data, item);
 	/* Ensure data is seen before seq number */
 	lf_fence_store();
 	/* Incrementing the sequence number tells the consumer the data
@@ -407,9 +415,13 @@ static bool lf_ring_enqueue_mpmc(lf_ring_mpmc *ring, void *item)
 }
 
 /* Same interface as other dequeue functions with a different implementation
- * to handle data races.
+ * to handle producer slot contention.
+ *
+ * This version is the exact same as the mpmc version, but we replace the
+ * CAS on ring->head with a relaxed store. See mpmc version for more comments
+ * on implementation.
  */
-static bool lf_ring_dequeue_mpmc(lf_ring_mpmc *ring, void **item)
+static bool lf_ring_mpsc_dequeue(lf_ring_mpmc *ring, lf_ring_slot_data *item)
 {
 	size_t h, h_next, seq, idx;
 	intptr_t diff_signed;
@@ -422,18 +434,53 @@ static bool lf_ring_dequeue_mpmc(lf_ring_mpmc *ring, void **item)
 		slot = &ring->buf[idx];
 		seq = lf_op_load_size(&slot->seq);
 
-		/* Compare sequence number and head as signed integers to take
-		 * wrap around into account.
-		 *
-		 * We use head + 1 because the consumer expects seq == slot_index
-		 * to indicate the slot is empty and seq == slot_index + 1 to indicate
-		 * there is data (see enqueue_mpmc).
-		 */
 		diff_signed = (intptr_t)seq - (intptr_t)h_next;
+		if (diff_signed == 0) {
+			/* Perform a relaxed store on head instead of CAS since no
+			 * other consumers can write to it.
+			 */
+			lf_op_store_size(&ring->head, h_next);
+		} else if (diff_signed > 0) {
+			continue;
+		} else {
+			return false;
+		}
+	}
+	lf_fence_load();
 
-		/* These cases are largely identical to the enqueue_mpmc ones with
-		 * a slightly different meaning.
-		 */
+	lf_ring_load_slot_data(&slot->data, item);
+	lf_fence_load_store();
+	lf_op_store_size(&slot->seq, h_next + ring->mask);
+	return true;
+}
+
+LF_ATTR_ALWAYS_INLINE
+static bool lf_ring_mpmc_init(lf_ring_mpmc *ring, lf_ring_mpmc_slot *buffer,
+			      size_t buffer_len)
+{
+	return lf_ring_mpsc_init(ring, buffer, buffer_len);
+}
+
+static bool lf_ring_mpmc_enqueue(lf_ring_mpmc *ring,
+				 const lf_ring_slot_data *item)
+{
+	return lf_ring_mpsc_enqueue(ring, item);
+}
+
+static bool lf_ring_mpmc_dequeue(lf_ring_mpmc *ring, lf_ring_slot_data *item)
+{
+	size_t h, h_next, seq, idx;
+	intptr_t diff_signed;
+	lf_ring_mpmc_slot *slot;
+
+	while (true) {
+		h = lf_op_load_size(&ring->head);
+		h_next = h + 1;
+		idx = h & ring->mask;
+		slot = &ring->buf[idx];
+		seq = lf_op_load_size(&slot->seq);
+
+		diff_signed = (intptr_t)seq - (intptr_t)h_next;
 		if (diff_signed == 0) {
 			if (lf_op_cas_size(&ring->head, h, h_next)) {
 				break;
@@ -444,23 +491,10 @@ static bool lf_ring_dequeue_mpmc(lf_ring_mpmc *ring, void **item)
 			return false;
 		}
 	}
-	/* Order with respect to previous loads to ensure we see most
-	 * recent data. (Might not be necessary)
-	 */
 	lf_fence_load();
 
-	*item = lf_op_load_ptr(&slot->data);
-	/* Ensure data is actually loaded before seq number is
-	 * updated.
-	 */
+	lf_ring_load_slot_data(&slot->data, item);
 	lf_fence_load_store();
-	/* Add cap to slot->seq to indicate slot is free.
-	 *
-	 * h = slot->seq, h_next = h + 1, cap = ring->mask + 1
-	 *
-	 * Using h_next seems weird but I used h + 1 3 times so
-	 * might as well store that and use it.
-	 */
 	lf_op_store_size(&slot->seq, h_next + ring->mask);
 	return true;
 }
